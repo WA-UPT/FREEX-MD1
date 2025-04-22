65630a1f10ffe58f8a4ed70c63f14a2fe9281544
DINU-FXMD <darkdenuwan5@gmail.com>
2025-04-22 17:22:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'DINU-MD=VPkG2a7R#WL_v_mdlGqYqlFfV05Eo36VvgAnkREnjHHl0_s5qWwo',
+SESSION_ID: process.env.SESSION_ID || 'DINU-MD=W8JmyBpL#TK_enIvUIm4nI5nqGOXGMV0mhtdKTbYKeX7aIRZgpLA',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 OWNER_NUMBER: process.env.OWNER_NUMBER || '94765527900', 
 PREFIX:  process.env.PREFIX || ['.'] ,
