314d669787f85090f8cf1e6351e1e69b405b55b1
DINU-FXMD <darkdenuwan5@gmail.com>
2025-04-23 17:17:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,11 +9,7 @@ SESSION_ID: process.env.SESSION_ID || 'SAHAS-MD=setzkCAI#nfsBrk0JdGwsAQPkUa7P87e
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 OWNER_NUMBER: process.env.OWNER_NUMBER || '94765527900', 
 PREFIX:  process.env.PREFIX || ['.'] ,
-JID: process.env.JID || "120363287634683059@newsletter",
-WACHLINK: process.env.WACHLINK || `https://whatsapp.com/channel/0029Vb8WOgSBPzjfG09WEn10`,
-FOOTER: '> *©ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀʜᴀꜱ & ᴅɪɴᴜᴡᴀ*',
-DIRECTION: true,
-LOGO: process.env.LOGO || `https://files.catbox.moe/siaqr5.jpg`,
+LOGO: process.env.LOGO || `https://i.ibb.co/RMxmMc5/4568.jpg`,
 OWNER_REACT: process.env.OWNER_REACT || 'true',
 AUTO_STATUS_READ: process.env.AUTO_STATUS_READ || "true",
 ANTI_CALL: process.env.ANTI_CALL || "true"
